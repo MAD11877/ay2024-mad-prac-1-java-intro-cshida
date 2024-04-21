7a4473f7dc0b3a20c9da1156bbb8a3e31198ade9
cshida <151449138+cshida@users.noreply.github.com>
2024-04-21 16:52:00 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -16,7 +16,7 @@ public class Question2
     System.out.print("Enter your weight in kilograms: ");
     double weight = in.nextDouble();
     double bmi = weight / (height * height);
-    System.out.println("Your BMI is " + bmi);
+    System.out.println("Your BMI is " + "%.1f", bmi);
     in.close();
   }
 }
