6aeacc216fb139207cedd63a17679bc77659e59b
cshida <151449138+cshida@users.noreply.github.com>
2024-04-21 17:02:47 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,7 +11,7 @@ public class Question3
      */
      
       Scanner in = new Scanner(System.in);
-      System.out.print("Enter your number: ");
+      //System.out.print("Enter your number: ");
       int num = in.nextInt();
       System.out.println(num * num);
       in.close();
